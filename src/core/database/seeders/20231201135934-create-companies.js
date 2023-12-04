063247baeb68a71ad66fbd78ea30b865304ccfa0
Eduardo Tejada <et3858@gmail.com>
2023-12-04 08:58:02 -0300

    Create json file for companies

--- src/core/database/seeders/20231201135934-create-companies.js
+++ src/core/database/seeders/20231201135934-create-companies.js
@@ -1,43 +1,24 @@
 'use strict';
 
+const fs = require('fs');
+
 const TABLE_NAME = 'companies';
-const PAYLOADS = [
-  {
-    name: 'Central SpA.',
-    dni: '22599907-4',
-    address: 'Calle Ignacio Carrera 1480, Santiago, Región Metropolitana',
-    phone_number: '+56613979869',
-  },
-  {
-    name: 'El Porteno SpA.',
-    dni: '12376402-1',
-    address: 'Paseo Los Almendros 92, Valparaiso, Valparaiso',
-    phone_number: '+56612118846',
-  },
-  {
-    name: 'Sociedad de alimentos Bío Bío Ltda.',
-    dni: '15897820-2',
-    address: 'Avenida Los Pioneros 355, Concepción, Bío Bío',
-    phone_number: '+56619889559',
-  },
-  {
-    name: 'Minera Oculta SpA.',
-    dni: '19473599-5',
-    address: 'Calle Las Rosas 780, Antofagasta, Antofagasta',
-    phone_number: '+56615943627',
-  },
-  {
-    name: 'Copa del Arbol Ltda.',
-    dni: '14128328-6',
-    address: 'Paseo Los Cerezos 65, Punta Arenas, Magallanes y de la Antártica Chilena',
-    phone_number: '+56612034211',
-  },
-];
+
+function getData() {
+  try {
+    const payloads = fs.readFileSync('src/core/database/data/companies.json');
+
+    return JSON.parse(payloads);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(TABLE_NAME, PAYLOADS, {});
+    const payloads = getData();
+    await queryInterface.bulkInsert(TABLE_NAME, payloads, {});
   },
 
   async down (queryInterface, Sequelize) {
