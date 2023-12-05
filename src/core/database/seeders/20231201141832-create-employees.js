ba3066c88bf7b06285c893f92fbe6cf1550edf74
Eduardo Tejada <et3858@gmail.com>
2023-12-05 08:24:40 -0300

    Create json file for employees

--- src/core/database/seeders/20231201141832-create-employees.js
+++ src/core/database/seeders/20231201141832-create-employees.js
@@ -1,48 +1,24 @@
 'use strict';
 
+const fs = require('fs');
+
 const TABLE_NAME = 'employees';
-const PAYLOADS = [
-  {
-    first_name: 'Roberto',
-    last_name: 'Sanhueza',
-    dni: '10445837-8',
-    email: 'calohev_uxi7@gmail.com',
-    company_id: 1,
-  },
-  {
-    first_name: 'Karen',
-    last_name: 'Araya',
-    dni: '24533915-1',
-    email: 'liyeluw-ulu87@mail.com',
-    company_id: 2,
-  },
-  {
-    first_name: 'Pablo',
-    last_name: 'Perez',
-    dni: '23392356-7',
-    email: 'zojasi_yiyi72@aol.com',
-    company_id: 5,
-  },
-  {
-    first_name: 'Sabrina',
-    last_name: 'Ochoa',
-    dni: '18564433-2',
-    email: 'fipoku-hoju45@gmail.com',
-    company_id: 4,
-  },
-  {
-    first_name: 'Lazlo',
-    last_name: 'Araneda',
-    dni: '8368016-4',
-    email: 'cucojot_upu19@gmail.com',
-    company_id: 2,
-  },
-];
+
+function getData() {
+  try {
+    const payloads = fs.readFileSync('src/core/database/data/employees.json');
+
+    return JSON.parse(payloads);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(TABLE_NAME, PAYLOADS, {});
+    const payloads = getData();
+    await queryInterface.bulkInsert(TABLE_NAME, payloads, {});
   },
 
   async down (queryInterface, Sequelize) {
